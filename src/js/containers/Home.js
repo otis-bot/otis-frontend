362cc652fa9b0748d92bf17a71a1d84fb6f1bd45
Michael <MichaelMerrittFarrell@gmail.com>
2016-06-04 14:41:11 -0400

    Fixed bootstrap row forrendering children post components

--- src/js/containers/Home.js
+++ src/js/containers/Home.js
@@ -25,7 +25,7 @@ class Home extends React.Component {
     const posts = this.props.posts.map((post, index) => <Post key={index} index={index} post={post} />);
 
     return (
-      <div>
+      <div className="row">
         <div className="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           {posts}
         </div>
